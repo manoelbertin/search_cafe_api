01db66e7fa69283daa626a27c8b79fde7e6e2119
manoel <manoeltirj@gmail.com>
2023-07-09 17:35:32 -0300

    Separeting those 2 methods: create rating and store.

--- app/controllers/api/v1/ratings_controller.rb
+++ app/controllers/api/v1/ratings_controller.rb
@@ -1,2 +1,15 @@
 class Api::V1::RatingsController < ApplicationController
+
+  def create
+    create_rating
+    create_store
+  end
+
+  private
+
+  def create_rating
+  end
+
+  def create_store
+  end
 end
