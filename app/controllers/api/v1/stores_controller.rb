b3d0ae8d14be6d2385f16321c88eb715b12b82e2
manoel <manoeltirj@gmail.com>
2023-07-09 17:10:05 -0300

    I'm filtering by the most loved ones, from the most loved.

--- app/controllers/api/v1/stores_controller.rb
+++ app/controllers/api/v1/stores_controller.rb
@@ -4,6 +4,7 @@ class Api::V1::StoresController < ApplicationController
   def index
     @stores = Store.within(params[:longitude].to_f, params[:latitude].to_f)
     .sort_by { |store| store.ratings_average }
+    .reverse
   end
   
   def show 
