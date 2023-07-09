4e091883c0b06a7b7c88bb665d44a6c5ff3c3c90
manoel <manoeltirj@gmail.com>
2023-07-09 17:52:38 -0300

    Createdthe create store method and after create the method create_rating.

--- app/controllers/api/v1/ratings_controller.rb
+++ app/controllers/api/v1/ratings_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::RatingsController < ApplicationController
 
   def create
-    create_rating
     create_store
+    create_rating
   end
 
   private
@@ -14,7 +14,12 @@ class Api::V1::RatingsController < ApplicationController
   end
 
   def create_store
-    @store = Store.find_or_create_by
+    @store = Store.find_or_create_by!(
+      lonlat: "POINT(#{params[:store][:longitude]}.to_f #{params[:store][:latitude]}.to_f)",
+      name: params[:store][:name],
+      address: params[:store][:address],
+      google_place_id: params[:store][:google_place_id]
+    )
   end
 
   def ratings_params
