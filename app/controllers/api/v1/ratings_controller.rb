137721ff87b227a54f600bb44fd76aed0935baf2
manoel <manoeltirj@gmail.com>
2023-07-09 17:58:13 -0300

    Formating float type both params: logitude and latitude.

--- app/controllers/api/v1/ratings_controller.rb
+++ app/controllers/api/v1/ratings_controller.rb
@@ -11,11 +11,12 @@ class Api::V1::RatingsController < ApplicationController
     @rating = Rating.new(ratings_params)
 
     @rating.store_id = @store.id
+    @rating.save!
   end
 
   def create_store
     @store = Store.find_or_create_by!(
-      lonlat: "POINT(#{params[:store][:longitude]}.to_f #{params[:store][:latitude]}.to_f)",
+      lonlat: "POINT(#{params[:store][:longitude].to_f} #{params[:store][:latitude].to_f})",
       name: params[:store][:name],
       address: params[:store][:address],
       google_place_id: params[:store][:google_place_id]
